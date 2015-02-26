aaca45da0314e00ee27d1e0067b98bc8b075f405
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-02-26 16:17:19 +0000

    Track increments in LazyComputation objects

--- pyop2/base.py
+++ pyop2/base.py
@@ -63,9 +63,10 @@ class LazyComputation(object):
     """Helper class holding computation to be carried later on.
     """
 
-    def __init__(self, reads, writes):
+    def __init__(self, reads, writes, incs):
         self.reads = set(flatten(reads))
         self.writes = set(flatten(writes))
+        self.incs = set(flatten(incs))
         self._scheduled = False
 
     def enqueue(self):
@@ -3474,7 +3475,7 @@ class Mat(SetAssociated):
 
         Called lazily after user calls :meth:`assemble`"""
         def __init__(self, mat):
-            super(Mat._Assembly, self).__init__(reads=mat, writes=mat)
+            super(Mat._Assembly, self).__init__(reads=mat, writes=mat, incs=mat)
             self._mat = mat
 
         def _run(self):
@@ -3787,7 +3788,8 @@ class ParLoop(LazyComputation):
     def __init__(self, kernel, iterset, *args, **kwargs):
         LazyComputation.__init__(self,
                                  set([a.data for a in args if a.access in [READ, RW]]) | Const._defs,
-                                 set([a.data for a in args if a.access in [RW, WRITE, MIN, MAX, INC]]))
+                                 set([a.data for a in args if a.access in [RW, WRITE, MIN, MAX, INC]]),
+                                 set([a.data for a in args if a.access in [INC]]))
         # INCs into globals need to start with zero and then sum back
         # into the input global at the end.  This has the same number
         # of reductions but means that successive par_loops
